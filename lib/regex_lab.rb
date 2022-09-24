bb37cdd8eff940a30b93a4d6f8bc9ae8b3dd7134
sheready-stl <stella.waithera@moringaschool.com>
2022-09-24 18:06:35 +0300

    done

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,38 @@
 def starts_with_a_vowel?(word)
+  if word.match(/(?<!\S)[aeiouAEIOU]/)
+      true
+  else
+      false
+  end
+
 
 end
 
-def words_starting_with_un_and_ending_with_ing(text)
 
+
+def words_starting_with_un_and_ending_with_ing(text)
+  text.scan(/[\w+un?]\w+ing/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
+# binding.pry
+
 def first_word_capitalized_and_ends_with_punctuation?(text)
+  if text.match(/^[A-Z][^?!.]*[?.!]$/)
+      true
+  else
+      false
+  end
 
 end
 
 def valid_phone_number?(phone)
-
+  if phone.grep(/^*[0-9]+[\W]*/)
+      true
+  else
+      false
+  end
 end
